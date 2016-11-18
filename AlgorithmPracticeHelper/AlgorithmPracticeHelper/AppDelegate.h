9f8bf7e782965b3cb7b4714eb789d862da0aeb5b
LILouis <lijunpeng91@gmail.com>
2016-11-18 05:20:41 -0500

    init projeect

---
+++ AlgorithmPracticeHelper/AlgorithmPracticeHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlgorithmPracticeHelper
+//
+//  Created by LILouis on 11/18/16.
+//  Copyright © 2016 LILouis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlgorithmPracticeHelper/AlgorithmPracticeHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlgorithmPracticeHelper
+//
+//  Created by LILouis on 11/18/16.
+//  Copyright © 2016 LILouis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
